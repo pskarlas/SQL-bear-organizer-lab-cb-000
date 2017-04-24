00cdd2597e3e52d355ffe31c89d351529b91f638
Panagiotis <pskarl@gmail.com>
2017-04-24 19:22:28 +0300

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE bears (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER,
+--   gender TEXT,
+--   color TEXT,
+--   temperament CHAR(1),
+--   alive BOOLEAN,
+-- );
+
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
